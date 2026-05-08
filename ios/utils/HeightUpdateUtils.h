ad22fe0de168a3281ced7ee763280b644ad3de11
Gregory Moskaliuk <mosckalyuck@gmail.com>
2026-05-08 11:25:39 +0200

    refactor: extract requestHeightUpdate into a shared C++ template (#307)

--- ios/utils/HeightUpdateUtils.h
+++ ios/utils/HeightUpdateUtils.h
@@ -1,6 +1,8 @@
 #pragma once
 #import "ENRMUIKit.h"
 #import <React/RCTUtils.h>
+#include <memory>
+#import <react/utils/ManagedObjectWrapper.h>
 
 /// Returns YES if the measured content height differs from the frame height
 /// Yoga assigned, comparing at physical-pixel granularity to avoid
@@ -11,4 +13,13 @@ static inline BOOL needsHeightUpdate(CGSize measuredSize, CGRect bounds)
   CGFloat assignedHeight = ceil(bounds.size.height * scale) / scale;
   CGFloat measuredHeight = ceil(measuredSize.height * scale) / scale;
   return assignedHeight != measuredHeight;
+}
+
+template <typename StateDataT, typename ConcreteStateT>
+inline void ENRMRequestHeightUpdate(std::shared_ptr<const ConcreteStateT> const &state, int &counter, id self)
+{
+  if (!state)
+    return;
+  counter++;
+  state->updateState(StateDataT(counter, facebook::react::wrapManagedObjectWeakly(self)));
 }
