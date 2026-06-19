0bcdcb03dac0cfa5938318e8a73d6eb5160df907
Angelika Serwa <angelika.serwa@swmansion.com>
2026-06-19 13:40:55 +0200

    chore: set up android example app

---
+++ apps/android-example/app/src/main/java/swmansion/enriched/markdown/android/example/AndroidExampleMainActivity.kt
@@ -0,0 +1,40 @@
+package swmansion.enriched.markdown.android.example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import com.swmansion.enriched.markdown.compose.ComposeModule
+
+class AndroidExampleMainActivity : ComponentActivity() {
+  @OptIn(ExperimentalMaterial3Api::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+
+    setContent {
+      MaterialTheme {
+        Scaffold(
+          topBar = {
+            TopAppBar(title = { Text("EnrichedMarkdownAndroidExample") })
+          }
+        ) { innerPadding ->
+          Text(
+            text = "Android Compose example (${ComposeModule::class.simpleName})",
+            modifier = Modifier
+              .fillMaxSize()
+              .padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
